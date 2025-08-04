9c18b4081610bbdca698ed70132ccb6708bdc268
yerinaLee <poikl11234@naver.com>
2025-08-04 22:31:21 +0900

    setting(ing)

---
+++ about-me/src/test/java/com/about/portfolio/AboutMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.about.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
